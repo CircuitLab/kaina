4cc117afb6a83256adfb06d41f700f1b3b5c3d60
nulltask <nulltask@gmail.com>
2012-06-13 20:39:35 +0900

    add support for servo

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -12,47 +12,19 @@
   var socket = exports.socket = io.connect();
   
   /**
-   * Charactor code and evnet pair.
-   */
-  
-  var charCode = {
-      98: 'handClose'
-    , 121: 'handOpen'
-    , 117: '1stUp'
-    , 110: '1stDown'
-    , 106: '2ndDown'
-    , 107: '2ndUp'
-    , 104: 'turnLeft'
-    , 108: 'turnRight'
-  };
-  
-  
-  /**
    * Bootstrap.
    */
   
   $(function() {
   
     /**
-     * Handle `keydown` and `keypress` event.
+     * Handle `change` event.
      */
     
-    $(document).on('keypress', function(e) {
-      var event = charCode[e.charCode];
-      if (event) {
-        socket.emit(event);
-      }
-    });
-    
-    /**
-     * Handle `keyup` event.
-     */
-    
-    $(document).on('keyup', function(e) {
-      var event = charCode[e.charCode];
-      if (event) {
-        socket.emit(event + ':stop');
-      }
+    $('[type=range]').on('change', function(e) {
+      var $target = $(e.target);
+      console.log($target.attr('id'), $target.val());
+      socket.emit($target.attr('id'), $target.val());
     });
     
     /**
--- socket.js
+++ socket.js
@@ -13,69 +13,16 @@ module.exports = function(app, arm) {
   });
   
   io.sockets.on('connection', function(socket) {
-    
-    /*
-    socket.on('handClose', function() {
-      arm.closeHand();
-    });
-    socket.on('handClose:stop', function() {
-      arm._off(arm.pins.closeHand);
-    });
-    */
-    
-    /*
-    socket.on('handOpen', function() {
-      arm.openHand();
-    });
-    socket.on('handOpen:stop', function() {
-      arm._off(arm.pins.openHand);
-    });
-    */
-    
-    socket.on('1stUp', function() {
-      arm.upFirst();
-    });
-    socket.on('1stUp:stop', function() {
-      arm._off(arm.pins.upFirst);
+    socket.on('wrist', function(data) {
+      arm.wrist(data);
     });
     
-    socket.on('1stDown', function() {
-      arm.downFirst();
-    });
-    socket.on('1stDown:stop', function() {
-      arm._off(arm.pins.downFirst);
+    socket.on('base', function(data) {
+      arm.base(data);
     });
     
-    socket.on('2ndUp', function() {
-      arm.upSecond();
-    });
-    socket.on('2ndUp:stop', function() {
-      arm._off(arm.pins.upSecond);
-    });
-    
-    socket.on('2ndDown', function() {
-      arm.downSecond();
-    });
-    socket.on('2ndDown:stop', function() {
-      arm._off(arm.pins.downSecond);
-    });
-    
-    socket.on('turnLeft', function() {
-      arm.turnLeft();
-    });
-    socket.on('turnLeft:stop', function() {
-      arm._off(arm.pins.turnLeft);
-    });
-    
-    socket.on('turnRight', function() {
-      arm.turnRight();
-    });
-    socket.on('turnRight:stop', function() {
-      arm._off(arm.pins.turnRight);
-    });
-    
-    socket.on('capture', function(data) {
-      io.sockets.volatile.emit('capture', data);
+    socket.on('elbow', function(data) {
+      arm.elbow(data);
     });
   });
   
