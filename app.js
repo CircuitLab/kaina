73027f1341e01abc3245677277e3ca5ceac0f41d
nulltask <nulltask@gmail.com>
2012-06-28 21:09:58 +0900

    Revert "tweak get method on web api"
    This reverts commit 016b816e8510ae37b9cd15c3ee29d61e37977d78.

--- app.js
+++ app.js
@@ -50,8 +50,10 @@ app.get('/arm/elbow', function(req, res) {
 
 app.post('/arm/elbow', function(req, res) {
   var val = req.body.val;
+  arm.servo.elbow.once('write', function() {
+    res.send({ res: 'success' });
+  });
   arm.wrist(val);
-  res.send({ res: 'success' });
 });
 
 app.get('/arm/wrist', function(req, res) {
@@ -63,8 +65,10 @@ app.get('/arm/wrist', function(req, res) {
 
 app.post('/arm/wrist', function(req, res) {
   var val = req.body.val;
+  arm.servo.wrist.once('write', function() {
+    res.send({ res: 'success' });
+  });
   arm.wrist(val);  
-  res.send({ res: 'success' });
 });
 
 /**
