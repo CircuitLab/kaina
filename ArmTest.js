b029049438a8bcc08067afbba0f87b8e62a05505
Rek Kawai <leicay@gmail.com>
2012-11-03 00:56:42 +0900

    refactoring

--- ArmTest.js
+++ ArmTest.js
@@ -1,16 +1,48 @@
+var asserts = [
+    {
+        'prev': 'ready'
+      , 'say': 'open hands'
+      , 'func': ['handsOpen']
+      , 'next': 'handsOpened'
+      , 'time': 500
+    }
+  , {
+        'prev': 'handsOpened'
+      , 'say': 'close hands'
+      , 'func': ['handsStop', 'handsClose']
+      , 'next': 'handsClosed'
+      , 'time': 500
+    }
+  , {
+        'prev': 'handsClosed'
+      , 'say': 'stop hands'
+      , 'func': ['handsStop']
+      , 'next': 'handsStoped'
+      , 'time': 500
+    }
+];
+
 var cp = require('child_process')
   , EventEmitter = require('events').EventEmitter;
 
-function ArmTest() {
+function ArmTest(asserts) {
   var Arm = require('./lib/arm');
-
   this.arm = new Arm(null, true);
 
-  //arm.handsOpen();
-  //setTimeout(arm.handsStop(), 1000);
-
-  console.log("test");
   var self = this;
+
+  for (var i = 0; i < asserts.length; i++) {
+    self.on(asserts[i].prev, (function(asserts, i) {
+      return function() {
+        cp.exec('say ' + asserts[i].say, function() {
+          for (var j = 0; j < asserts[i].func.length; j++) {
+            self.arm[asserts[i].func[j]]();
+          }
+          setTimeout(function(){ armTest.emit(asserts[i].next); }, asserts[i].time);
+        }
+      )}
+    })(asserts, i));
+  }
 
   this.arm.on('ready', function() {
     self.emit('ready');
@@ -19,33 +51,4 @@ function ArmTest() {
 
 ArmTest.prototype.__proto__ = EventEmitter.prototype;
 
-var armTest = new ArmTest();
-
-armTest.on('ready', function() {
-  console.log('arm ready');
-  cp.exec('say "open hands"', function() {
-    console.log('open hands');
-    armTest.arm.handsStop();
-    armTest.arm.handsOpen();
-    setTimeout(function(){ armTest.emit('handsOpened'); }, 500);
-  });
-});
-
-armTest.on('handsOpened', function() {
-  console.log('hands opened');
-  cp.exec('say "close hands"', function() {
-    console.log('close hands');
-    armTest.arm.handsStop();
-    armTest.arm.handsClose();
-    setTimeout(function(){ armTest.emit('handsClosed'); }, 500);
-  });
-});
-
-armTest.on('handsClosed', function() {
-  console.log('hands closed');
-  cp.exec('say "stop hands"', function() {
-    console.log('stop hands');
-    armTest.arm.handsStop();
-    setTimeout(function(){ armTest.emit('handsStoped'); }, 500);
-  });
-});
+var armTest = new ArmTest(asserts);
