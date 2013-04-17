81742e8a30cb6a173f00b6f7e6c43cd8df1889ab
Rei Kawai <leicay@gmail.com>
2013-04-17 23:04:43 +0900

    調整

--- ArmTest.js
+++ ArmTest.js
@@ -22,12 +22,12 @@ var asserts = [
   , {
         'say': 'up elbow'
       , 'func': ['elbowUp']
-      , 'time': 250
+      , 'time': 500
     }
   , {
         'say': 'down elbow'
       , 'func': ['elbowStop', 'elbowDown']
-      , 'time': 250
+      , 'time': 500
     }
   , {
         'say': 'stop elbow'
@@ -37,12 +37,12 @@ var asserts = [
   , {
         'say': 'up shoulder'
       , 'func': ['shoulderUp']
-      , 'time': 250
+      , 'time': 500
     }
   , {
         'say': 'down shoulder'
       , 'func': ['shoulderStop', 'shoulderDown']
-      , 'time': 250
+      , 'time': 500
     }
   , {
         'say': 'stop shoulder'
@@ -52,12 +52,12 @@ var asserts = [
   , {
         'say': 'turn right'
       , 'func': ['baseRotate']
-      , 'time': 500
+      , 'time': 1000
     }
   , {
         'say': 'turn left'
       , 'func': ['baseStop', 'baseReverse']
-      , 'time': 500
+      , 'time': 1000
     }
   , {
         'say': 'stop all'
@@ -65,6 +65,75 @@ var asserts = [
       , 'time': 250
     }
 ];
+/*
+var asserts = [
+    {
+        'say': "Let\\'s Dancing!!"
+      , 'func': []
+      , 'time': 200
+    }
+  , {
+        'func': ['handsOpen', 'elbowUp']
+      , 'time': 3000
+    }
+  , {
+        'func': ['handsStop', 'elbowStop', 'shoulderUp', 'baseRotate']
+      , 'time': 3000
+    }
+  , {
+        'func': ['shoulderStop', 'baseStop', 'handsClose', 'elbowDown', 'baseReverse']
+      , 'time': 3000
+    }
+  , {
+        'func': ['handsStop', 'elbowStop', 'baseStop']
+      , 'time': 200
+    }
+  , {
+        'func': ['shoulderUp', 'elbowDown']
+      , 'time': 1000
+    }
+  , {
+        'func': ['shoulderStop', 'elbowStop']
+      , 'time': 250
+    }
+  , {
+        'func': ['shoulderUp', 'elbowDown']
+      , 'time': 1000
+    }
+  , {
+        'func': ['shoulderStop', 'elbowStop']
+      , 'time': 250
+    }
+  , {
+        'func': ['shoulderUp', 'elbowDown']
+      , 'time': 1000
+    }
+  , {
+        'func': ['shoulderStop', 'elbowStop']
+      , 'time': 200
+    }
+  , {
+        'func': ['shoulderDown', 'elbowUp']
+      , 'time': 1000
+    }
+  , {
+        'func': ['shoulderStop', 'elbowStop']
+      , 'time': 250
+    }
+  , {
+        'func': ['shoulderDown', 'elbowUp']
+      , 'time': 1000
+    }
+  , {
+        'func': ['shoulderStop', 'elbowStop']
+      , 'time': 250
+    }
+  , {
+        'func': ['shoulderDown', 'elbowUp']
+      , 'time': 1000
+    }
+];
+*/
 
 var Arm = require('./lib/arm');
 var arm = new Arm(null, true);
--- SayDo.js
+++ SayDo.js
@@ -14,14 +14,23 @@ function SayDo(opts) {
 
   for (i = 0; i < asserts.length; i++) {
     self.on(i == 0 ? 'ready' : eventPrefix + i, (function(asserts, i) {
-      return function() {
-        cp.exec('say ' + asserts[i].say, function() {
+      if (asserts[i].say) {
+        return function() {
+          cp.exec('say ' + asserts[i].say, function() {
+            for (var j = 0; j < asserts[i].func.length; j++) {
+              self.target[asserts[i].func[j]]();
+            }
+            setTimeout(function(){ self.emit(eventPrefix + (i + 1)); }, asserts[i].time);
+          }
+        )}
+      } else {
+        return function() {
           for (var j = 0; j < asserts[i].func.length; j++) {
             self.target[asserts[i].func[j]]();
           }
           setTimeout(function(){ self.emit(eventPrefix + (i + 1)); }, asserts[i].time);
         }
-      )}
+      }
     })(asserts, i));
   }
   self.on(eventPrefix + i, function() { process.exit(); });
